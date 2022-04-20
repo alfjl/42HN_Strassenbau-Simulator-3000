0790776c74c8a6901191300154a62975beaaacf0
coder <you@example.com>
2022-04-20 10:07:12 +0200

    cleaned up player.c

--- src/player.c
+++ src/player.c
@@ -1,45 +1,41 @@
 #include "cub3d.h"
 
-static void	static_player_update(float *straight, float dstraight, float *cross,
-	float dcross)
+static void	static_player_update(float *x, float dx, float *y, float dy)
 {
 	float	new_x;
 	float	new_y;
 
-	new_x = *straight + dstraight;
-	new_y = *cross + dcross;
-	if (!is_wall(*cross,
-			(*straight + dstraight)))
+	new_x = *x + dx;
+	new_y = *y + dy;
+	if (!is_wall(*y, (*x + dx)))
 	{
 		if (new_x >= 0 && new_x < data()->map.width)
-			*straight = new_x;
+			*x = new_x;
 	}
-	if (!is_wall((*cross + dcross),
-			*straight))
+	if (!is_wall((*y + dy), *x))
 	{
 		if (new_y >= 0 && new_y < data()->map.height)
-			*cross = new_y;
+			*y = new_y;
 	}
 }
 
 void	player_update_position(t_keys *keys)
 {
-	data()->player.dx = cos(data()->player.angle) * STEP;
-	data()->player.dy = sin(data()->player.angle) * STEP;
+	t_player	*player;
+
+	player = &data()->player;
+	player->dx = cos(player->angle) * STEP;
+	player->dy = sin(player->angle) * STEP;
 	if (keys->forwards)
-		static_player_update(&data()->player.x, data()->player.dx,
-			&data()->player.y, data()->player.dy);
+		static_player_update(&player->x, player->dx, &player->y, player->dy);
 	if (keys->backwards)
-		static_player_update(&data()->player.x, -data()->player.dx,
-			&data()->player.y, -data()->player.dy);
+		static_player_update(&player->x, -player->dx, &player->y, -player->dy);
 	if (keys->leftwards)
-		static_player_update(&data()->player.x, data()->player.dy,
-			&data()->player.y, -data()->player.dx);
+		static_player_update(&player->x, player->dy, &player->y, -player->dx);
 	if (keys->rightwards)
-		static_player_update(&data()->player.x, -data()->player.dy,
-			&data()->player.y, data()->player.dx);
+		static_player_update(&player->x, -player->dy, &player->y, player->dx);
 	if (keys->turnleft)
-		data()->player.angle = radian_limits(data()->player.angle - STEP_A);
+		player->angle = radian_limits(player->angle - STEP_A);
 	if (keys->turnright)
-		data()->player.angle = radian_limits(data()->player.angle + STEP_A);
+		player->angle = radian_limits(player->angle + STEP_A);
 }
