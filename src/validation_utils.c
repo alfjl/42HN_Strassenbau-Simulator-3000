d93430ea43b2fd171edc5cae3fc27b9ab5d97817
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-06-01 09:53:46 +0200

    Commit before pull

--- src/validation_utils.c
+++ src/validation_utils.c
@@ -6,13 +6,59 @@
 /*   By: alanghan <alanghan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 18:17:17 by alanghan          #+#    #+#             */
-/*   Updated: 2022/04/30 19:15:17 by alanghan         ###   ########.fr       */
+/*   Updated: 2022/05/18 15:55:20 by alanghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 /* ----------------------------- FUNC 1 ------------------------------------ */
+bool	validation_typecheck_cub_extension(unsigned int length,
+											char **sections, int i)
+{
+	if (i > 0)
+		i -= 1;
+	if (
+		length < 5
+		|| sections[i][length - 4] != '.'
+		|| sections[i][length - 3] != 'c'
+		|| sections[i][length - 2] != 'u'
+		|| sections[i][length - 1] != 'b'
+	)
+		return (false);
+	else
+		return (true);
+}
+
+/* ----------------------------- FUNC 2 ------------------------------------ */
+bool	validation_typecheck_cub(const char *path)
+{
+	bool			return_value;
+	int				i;
+	unsigned int	length;
+	char			**sections;
+
+	if (path == NULL)
+		return (false);
+	sections = ft_split(path, '/');
+	if (sections == NULL)
+		return (false);
+	i = 0;
+	while (sections[i])
+	{
+		length = ft_strlen(sections[i]);
+		i++;
+	}
+	return_value = validation_typecheck_cub_extension(length, sections, i);
+	while (i >= 0)
+	{
+		free(sections[i]);
+		i--;
+	}
+	free(sections);
+	return (return_value);
+}
+
 bool	validation_typecheck_cub(const char *path)
 {
 	unsigned int	length;
@@ -32,7 +78,7 @@ bool	validation_typecheck_cub(const char *path)
 		return (true);
 }
 
-/* ----------------------------- FUNC 2 ------------------------------------ */
+/* ----------------------------- FUNC 3 ------------------------------------ */
 bool	validation_typecheck_xpm(const char *path)
 {
 	unsigned int	length;
@@ -52,7 +98,7 @@ bool	validation_typecheck_xpm(const char *path)
 		return (true);
 }
 
-/* ------------------------------ FUNC 3 ----------------------------------- */
+/* ------------------------------ FUNC 4 ----------------------------------- */
 bool	validation_check_boolian(bool boolian, int *errorcode, int code)
 {
 	if (boolian == true)
@@ -61,7 +107,7 @@ bool	validation_check_boolian(bool boolian, int *errorcode, int code)
 	return (false);
 }
 
-/* ------------------------------ FUNC 4 ----------------------------------- */
+/* ------------------------------ FUNC 5 ----------------------------------- */
 unsigned int	calculate_rgb_value(unsigned int *rgb, unsigned int r,
 										unsigned int g, unsigned int b)
 {
